a1ce56d614c8875b595f59aa597a90cdcdc72e6d
DrWatson <no@mail>
2021-07-27 19:07:21 +0200

    Restore JPetalo.jl

---
+++ src/JPetalo.jl
@@ -0,0 +1,12 @@
+module JPetalo
+include("types.jl")
+include("util.jl")
+include("math.jl")
+include("io.jl")
+include("ana.jl")
+include("nemareco.jl")
+include("fits.jl")
+include("dftolor.jl")
+include("plots.jl")
+include("histos.jl")
+end

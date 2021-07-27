943e6eae760ee0f0dab2cda7acb1321b50e51df9
DrWatson <no@mail>
2021-07-27 10:20:53 +0200

    Use parametric types in all relevant math function.

--- src/math.jl
+++ src/math.jl
@@ -2,7 +2,7 @@ using DataFrames
 using StatsBase
 
 """
-	rxy(x::Number,y::Number)
+	rxy(x::T,y::T) where T
 	r -> sqrt{x^2 + y^2}
 """
 function rxy(x::T,y::T) where T
@@ -11,7 +11,7 @@ end
 
 
 """
-	phixy(x::Number,y::Number)
+	hixy(x::T,y::T) where T
 	phi -> atan(y/x)
 """
 function phixy(x::T,y::T) where T
@@ -19,8 +19,8 @@ function phixy(x::T,y::T) where T
 end
 
 """
-	phixy(x::Number,y::Number)
-	phi -> atan(y/x)
+	phixy(hitdf::DataFrame)
+	phi -> atan(y/x) where y and x are columns of the data frame
 """
 function fphi(hitdf::DataFrame)
     return atan.(hitdf.y,hitdf.x)
@@ -28,7 +28,7 @@ end
 
 
 """
-	dxyz(x1::Vector{Number}, x2::Vector{Number})
+	dxyz(x1::Vector{T}, x2::Vector{T})
 
 Distance between two points.
 """
@@ -38,32 +38,32 @@ function dxyz(x1::Vector{T}, x2::Vector{T}) where T
 end
 
 """
-function gwstd(x, q)
+function wstd(x::Vector{T}, q::Vector{T}) where T
 
 Compute the std deviation in x weighted by q:
 Sqrt(1/Q Sum_i (x - x_mean) * qi )
 """
-function gwstd(x, q)
+function wstd(x::Vector{T}, q::Vector{T}) where T
 	xmean = mean(x)
 	qs = sum((x.-xmean).^2 .* q)
 	Q = sum(q)
 	return sqrt(qs/Q)
 end
-wstd(x::Vector{Float32}, q::Vector{Float32}) = gwstd(x, q)
-wstd(x::Vector{Float64}, q::Vector{Float64}) = gwstd(x, q)
-wstd(x::Vector{Number}, q::Vector{Number}) = gwstd(x, q)
+#wstd(x::Vector{Float32}, q::Vector{Float32}) = gwstd(x, q)
+#wstd(x::Vector{Float64}, q::Vector{Float64}) = gwstd(x, q)
+#wstd(x::Vector{Number}, q::Vector{Number}) = gwstd(x, q)
 
 
 """
 	mean_std(x, xmin, xmax)
 	Returns mean and std for a vector x in the interval between xmin and xmax
 """
-function gmean_std(x, xmin, xmax)
+function mean_std(x::Vector{Float32}, xmin::T, xmax::T)
     xx = in_range(x, xmin, xmax)
     xm = mean(xx)
     xs = std(xx)
     return xm, xs
 end
-mean_std(x::Vector{Float32}, xmin::Float32, xmax::Float32) = gmean_std(x, xmin, xmax)
-mean_std(x::Vector{Float64}, xmin::Float64, xmax::Float64) = gmean_std(x, xmin, xmax)
-mean_std(x::Vector{Int64}, xmin::Int64, xmax::Int64) = gmean_std(x, xmin, xmax)
+#mean_std(x::Vector{Float32}, xmin::Float32, xmax::Float32) = gmean_std(x, xmin, xmax)
+#mean_std(x::Vector{Float64}, xmin::Float64, xmax::Float64) = gmean_std(x, xmin, xmax)
+#mean_std(x::Vector{Int64}, xmin::Int64, xmax::Int64) = gmean_std(x, xmin, xmax)
